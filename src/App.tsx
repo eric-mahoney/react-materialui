53364b04eac373100a0b739e8df7dfb07f997717
ericmahoney <eric.mahoney22@gmail.com>
2021-03-16 01:18:52 -0400

    added drawer with search icon and text

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Drawer from "@material-ui/core/Drawer";
+import { List, ListItem, ListItemIcon, ListItemText } from "@material-ui/core";
+import { Search } from "@material-ui/icons";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Drawer variant="permanent" anchor="left">
+        <List>
+          <ListItem button>
+            <ListItemIcon>
+              <Search />
+            </ListItemIcon>
+            <ListItemText primary="Search" />
+          </ListItem>
+        </List>
+      </Drawer>
     </div>
   );
 }
